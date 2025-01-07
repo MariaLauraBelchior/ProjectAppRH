b395c2249fd92203521904dad06f7c1854cb5686
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2025-01-07 17:09:57 -0300

    Correção do controller dependetes

--- src/main/java/com/ApplicationRH/AppRH/controller/DependenteController.java
+++ src/main/java/com/ApplicationRH/AppRH/controller/DependenteController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,11 +28,13 @@ public class DependenteController {
         return ResponseEntity.ok(nDependente);
     }
     
+    @GetMapping
     public ResponseEntity<List<DependenteDTO>> findAll() {
         List<DependenteDTO> list = dependenteService.findAll();
         return ResponseEntity.ok(list);
     } 
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<DependenteDTO> deletarDependente(@PathVariable Long id) {
         dependenteService.deletarDependente(id);
         return ResponseEntity.noContent().build();
